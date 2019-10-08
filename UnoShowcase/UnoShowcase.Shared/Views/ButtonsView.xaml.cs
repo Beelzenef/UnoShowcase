fde9e0596ee22d12808c31b1a839a001da76dae3
Elena Guzman <elena.guzbla@gmail.com>
2019-10-08 15:30:16 +0200

    :sparkles: Add buttons shorcase

---
+++ UnoShowcase/UnoShowcase.Shared/Views/ButtonsView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace UnoShowcase.Shared.Views
+{
+    public sealed partial class ButtonsView : Page
+    {
+        public ButtonsView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GoBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
